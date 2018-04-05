164aacd81281d8d756aa0c3912ae103abc7d3213
Sandy-Hinchcliffe <sandy.hinchcliffe@bath.edu>
2018-04-05 12:21:49 +0100

    Added map class

---
+++ Map.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Map1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MapClass test = new MapClass();
+            Console.WriteLine(test.TreasureDist(1, 1, 0, 0));
+        }
+    }
+}
